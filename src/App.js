f62812726af11f489280d6dd70312266e54f7ad5
shayan heidary <shayanheidary@shayans-MacBook-Pro.local>
2023-07-15 12:26:15 +0200

    fire base installed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "../src/categories.styles.scss";
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
-
+import SignIn from "./routes/sign-in/sign-in.component";
 const Shop = () => {
   return <h1>i am shop page</h1>;
 };
@@ -11,9 +11,10 @@ const Shop = () => {
 function App() {
   return (
     <Routes>
-      <Route path="" element={<Navigation />}>
+      <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="signin" element={<SignIn />} />
       </Route>
     </Routes>
   );
