3fadcc6e578436edfc72597473d99e6e5fea2133
shayan heidary <shayanheidary@shayans-MacBook-Pro.local>
2023-07-16 15:46:17 +0200

    sign in with google redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,36 @@
 import {
+  auth,
   signInWithGooglePopup,
   creatUseDocumentFromAuth,
+  signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.util";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRefrence = await creatUseDocumentFromAuth(user);
   };
+
+  useEffect(() => {
+    const func = async () => {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRefrence = await creatUseDocumentFromAuth(response.user);
+      }
+    };
+
+    func();
+  }, []);
+
   return (
     <div>
       <h1>Sign in page</h1>
       <button onClick={logGoogleUser}>Log in with google account</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Log in with google redirect account
+      </button>
     </div>
   );
 };
